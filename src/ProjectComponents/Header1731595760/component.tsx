4d1ddd568f8325afaa382a900546aa822f8295bf
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-11-21 11:08:29 +0100

    feat: Edit Header1731595760
    Requested change: Add a title saying 'WII'

--- src/ProjectComponents/Header1731595760/component.tsx
+++ src/ProjectComponents/Header1731595760/component.tsx
@@ -5,6 +5,7 @@ const Header: React.FC = () => {
     <header className="bg-blue-500 text-white p-4 w-full h-full"> {/* Full width and height */}
       <div className="container mx-auto flex justify-between items-center h-full">
         <div className="text-2xl font-bold">AquaVessel</div>
+        <div className="text-3xl font-extrabold">WII</div>
       </div>
     </header>
   );
